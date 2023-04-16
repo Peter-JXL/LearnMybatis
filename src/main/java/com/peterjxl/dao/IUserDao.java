83bda4b7ee0b83df314ed166a228d02ce69586c6
peterjxl <peterjxl@qq.com>
2023-04-16 16:21:01 +0800

    :tada: Mybatis入门案例-使用注解

--- src/main/java/com/peterjxl/dao/IUserDao.java
+++ src/main/java/com/peterjxl/dao/IUserDao.java
@@ -1,6 +1,8 @@
 package com.peterjxl.dao;
 
 import com.peterjxl.domain.User;
+import org.apache.ibatis.annotations.Select;
+
 import java.util.List;
 
 public interface IUserDao {
@@ -9,5 +11,6 @@ public interface IUserDao {
      * 查询所有用户
      * @return
      */
+    @Select("select * from user")
     List<User> findAll();
 }
